7244b047d8d967a99633eff10b9baa8d6b194921
JongBaaam <JongBaaam@gmail.com>
2024-09-18 15:44:30 +0900

    Chore: 초기 보일러플레이트 생성

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.json({ message: err.message });
+});
+
+module.exports = app;
