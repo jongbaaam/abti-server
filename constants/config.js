b97c46f60f8641a998e664293c304c73afc1853a
JongBaaam <JongBaaam@gmail.com>
2024-11-05 19:53:51 +0900

    Feat: 서버 도메인 환경 변수 추가

--- constants/config.js
+++ constants/config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const CONFIG = {
   DATABASE_URL: process.env.DATABASE_URL,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+  SERVER_DOMAIN: process.env.SERVER_DOMAIN,
 };
 
 module.exports = CONFIG;
